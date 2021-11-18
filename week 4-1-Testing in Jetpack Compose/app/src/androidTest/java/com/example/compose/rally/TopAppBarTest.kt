b5eb78a4b9a8ecd2b1c60b0d71f671ec5c621e39
qwerop553 <qwerop553@gmail.com>
2021-11-18 16:34:30 +0900

    4-1 ~6까지 완료

---
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -0,0 +1,57 @@
+package com.example.compose.rally
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.text.toUpperCase
+import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.theme.RallyTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists(){
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = {},
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
+
+        composeTestRule
+            .onNode(hasText(RallyScreen.Accounts.name.uppercase()) and
+            hasParent(
+                hasContentDescription(RallyScreen.Accounts.name)
+            ),
+            useUnmergedTree = true)
+            .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_isNavigatingWorksWell(){
+
+        composeTestRule.setContent{
+            RallyTheme{
+                RallyApp()
+            }
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("isNavigatingWorksWell")
+
+        composeTestRule.onNode(hasStateDescription("currentscreen"))
+            .assertExists()
+    }
+}
